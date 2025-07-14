cdcf582ca6f6daf35258ed3d3bb1a60f89a1f940
Shivkumarumar519 <143341202+Shivkumarumar519@users.noreply.github.com>
2025-07-14 12:32:43 +0530

    Update EmailServiceImpl.java

--- src/main/java/com/smc/services/impl/EmailServiceImpl.java
+++ src/main/java/com/smc/services/impl/EmailServiceImpl.java
@@ -18,50 +18,12 @@ import jakarta.mail.internet.MimeMessage;
 public class EmailServiceImpl implements EmailService{
 
 
-	/*
-	 * @Override public boolean sendEmail(String to, String body, String subject) {
-	 * boolean f = false;
-	 * 
-	 * String from = "smartcontactmanager2025@gmail.com"; String host =
-	 * "smtp.gmail.com"; Properties properties = System.getProperties();
-	 * properties.put("mail.smtp.host", host); properties.put("mail.smtp.port",
-	 * "465"); properties.put("mail.smtp.ssl.enable", "true");
-	 * properties.put("mail.smtp.auth", "true");
-	 * 
-	 * // Get session Session session = Session.getInstance(properties, new
-	 * Authenticator() {
-	 * 
-	 * @Override protected PasswordAuthentication getPasswordAuthentication() {
-	 * //return new PasswordAuthentication("shivkumarumar519@gmail.com",
-	 * "yezzkjhzlxszehnr"); return new
-	 * PasswordAuthentication("smartcontactmanager2025@gmail.com",
-	 * "zbfpiqwjsokndrlp"); } });
-	 * 
-	 * session.setDebug(true);
-	 * 
-	 * MimeMessage mimeMessage = new MimeMessage(session); try {
-	 * mimeMessage.setFrom(from);
-	 * 
-	 * // Ensure 'to' is a valid email address
-	 * System.out.println("Sending email to: " + to);
-	 * mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-	 * 
-	 * mimeMessage.setSubject(body); mimeMessage.setContent(subject, "text/html");
-	 * 
-	 * Transport.send(mimeMessage); System.out.println("Email sent successfully.");
-	 * f = true; } catch (Exception e) { e.printStackTrace(); } return f; }
-	 * 
-	 * @Override public void sendEmailwithHtml() { // TODO Auto-generated method
-	 * stub
-	 * 
-	 * }
-	 */
 	
 	@Override
 	public boolean sendEmail(String to, String subject, String body) {
 	    boolean f = false;
 
-	    String from = "smartcontactmanager2025@gmail.com";
+	    String from = "your_email@gmail.com";
 	    String host = "smtp.gmail.com";
 	    Properties properties = System.getProperties();
 	    properties.put("mail.smtp.host", host);
@@ -73,7 +35,7 @@ public class EmailServiceImpl implements EmailService{
 	    Session session = Session.getInstance(properties, new Authenticator() {
 	        @Override
 	        protected PasswordAuthentication getPasswordAuthentication() {
-	            return new PasswordAuthentication("smartcontactmanager2025@gmail.com", "bitcuwdpaxosdgsl");
+	            return new PasswordAuthentication("your_email@gmail.com", "your_secret_key");
 	        }
 	    });
 
